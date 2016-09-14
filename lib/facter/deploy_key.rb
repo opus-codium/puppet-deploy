b52f3b54025a2419856a920ba83755cf0f8d29d0
Romain Tartière <romain@blogreen.org>
2016-09-14 19:29:42 +0200

    Make facter export system accounts' public SSH key

---
+++ lib/facter/deploy_key.rb
@@ -0,0 +1,13 @@
+require 'facter'
+
+`/usr/bin/getent passwd`.each_line do |line|
+  login, _passwd, uid, _gid, _gecos, home, _shell = line.split(':')
+  next unless uid.to_i < 1000
+
+  rsa_pub_key = "#{home}/.ssh/id_rsa.pub"
+  next unless File.exist?(rsa_pub_key)
+
+  Facter.add("#{login}_key") do
+    File.read(rsa_pub_key)
+  end
+end
