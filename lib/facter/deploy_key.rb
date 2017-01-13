f1b326ce1b2e99f061a1b36ad4d5888180731c24
Romain Tartière <romain@blogreen.org>
2017-01-13 13:39:50 +0100

    Confine facts to systems where getent is available

--- lib/facter/deploy_key.rb
+++ lib/facter/deploy_key.rb
@@ -1,15 +1,17 @@
 require 'facter'
 
-`/usr/bin/getent passwd`.each_line do |line|
-  login, _passwd, uid, _gid, _gecos, home, _shell = line.force_encoding('UTF-8').split(':')
-  next unless uid.to_i < 1000
+if File.executable?('/usr/bin/getent')
+  `/usr/bin/getent passwd`.each_line do |line|
+    login, _passwd, uid, _gid, _gecos, home, _shell = line.force_encoding('UTF-8').split(':')
+    next unless uid.to_i < 1000
 
-  rsa_pub_key = "#{home}/.ssh/id_rsa.pub"
-  next unless File.exist?(rsa_pub_key)
+    rsa_pub_key = "#{home}/.ssh/id_rsa.pub"
+    next unless File.exist?(rsa_pub_key)
 
-  Facter.add("#{login}_key") do
-    setcode do
-      File.read(rsa_pub_key)
+    Facter.add("#{login}_key") do
+      setcode do
+        File.read(rsa_pub_key)
+      end
     end
   end
 end
