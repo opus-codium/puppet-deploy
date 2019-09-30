fbc69d19ee3f541b23531bb02353030e35951949
Romain Tartière <romain@blogreen.org>
2019-09-30 08:27:03 -1000

    Report all users ssh public keys
    Not only system users ssh public keys.

--- lib/facter/deploy_key.rb
+++ lib/facter/deploy_key.rb
@@ -2,8 +2,7 @@ require 'facter'
 
 if File.executable?('/usr/bin/getent')
   `/usr/bin/getent passwd`.each_line do |line|
-    login, _passwd, uid, _gid, _gecos, home, _shell = line.force_encoding('UTF-8').split(':')
-    next unless uid.to_i < 1000
+    login, _passwd, _uid, _gid, _gecos, home, _shell = line.force_encoding('UTF-8').split(':')
 
     rsa_pub_key = "#{home}/.ssh/id_rsa.pub"
     next unless File.exist?(rsa_pub_key)
